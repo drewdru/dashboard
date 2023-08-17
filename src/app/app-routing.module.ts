afca9e341d66401858e7e1d2df9dae454d0bf966
Drew Dru <drew.drux@gmail.com>
2023-08-17 10:25:37 +0400

    fix router for github pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', component: DashboardComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
 
